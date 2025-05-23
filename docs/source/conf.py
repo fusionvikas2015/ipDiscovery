35ca1c873693efb5320666de9f91ba54cb425daf
Vikas Kumar <pythonsikho@gmail.com>
2025-05-24 02:59:45 +0530

    upadte docs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -30,7 +30,7 @@ exclude_patterns = []
 
 # html_theme = 'alabaster'
 html_theme = "sphinx_rtd_theme"
-html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
+# html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 
 # html_theme_options = {
 #     "collapse_navigation": False,
